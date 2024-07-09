31fdc68a52614a0451615c6480ea50b44c74ee16
goosebbeoms <seo980916@gmail.com>
2024-07-09 21:41:47 +0900

    214. State(상태) 스케줄링 & 배칭

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -28,38 +28,31 @@ function isPrime(number) {
 const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
 
-  const initialCountIsPrime = useMemo(
-    () => isPrime(initialCount),
-    [initialCount]
-  );
+  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
   // useEffect(() => {
   //   setCounterChanges([{ value: initialCount, id: Math.random() * 1000}])
   // }, [initialCount])
 
   // const [counter, setCounter] = useState(initialCount);
-  const [counterChanges, setCounterChanges] = useState([{ value: initialCount, id: Math.random() * 1000}]);
+  const [counterChanges, setCounterChanges] = useState([{ value: initialCount, id: Math.random() * 1000 }]);
 
-  const currentCounter = counterChanges.reduce(
-    (prevCounter, counterChange) => prevCounter + counterChange.value,
-    0
-  );
+  const currentCounter = counterChanges.reduce((prevCounter, counterChange) => prevCounter + counterChange.value, 0);
 
   const handleDecrement = useCallback(function handleDecrement() {
     // setCounter((prevCounter) => prevCounter - 1);
-    setCounterChanges((prevCounterChanges) => [{ value: -1, id: Math.random() * 1000}, ...prevCounterChanges]);
+    setCounterChanges((prevCounterChanges) => [{ value: -1, id: Math.random() * 1000 }, ...prevCounterChanges]);
   }, []);
 
   const handleIncrement = useCallback(function handleIncrement() {
     // setCounter((prevCounter) => prevCounter + 1);
-    setCounterChanges((prevCounterChanges) => [{ value: 1, id: Math.random() * 1000}, ...prevCounterChanges]);
+    setCounterChanges((prevCounterChanges) => [{ value: 1, id: Math.random() * 1000 }, ...prevCounterChanges]);
   }, []);
 
   return (
     <section className="counter">
       <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{' '}
-        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+        The initial counter value was <strong>{initialCount}</strong>. It <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
       </p>
       <p>
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
